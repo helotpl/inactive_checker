6f87865d575f8162a8833425f275564b85a83f39
helotpl <barczyk@gmail.com>
2022-02-24 15:35:28 +0100

    2nd

--- inactive_checker.go
+++ inactive_checker.go
@@ -5,8 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"github.com/antchfx/xmlquery"
+	"github.com/helloyi/go-sshclient"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 )
 
@@ -62,12 +65,79 @@ func remove(slice []string, s int) []string {
 	return append(slice[:s], slice[s+1:]...)
 }
 
+type SSHClient struct {
+	User       string `yaml:"user"`
+	Pass       string `yaml:"pass,omitempty"`
+	KeyFile    string `yaml:"key-file,omitempty"`
+	NumWorkers *int   `yaml:"num-workers"`
+}
+
+type Config struct {
+	SSHClient SSHClient `yaml:"ssh-client"`
+	SSHHosts  []string  `yaml:"ssh-hosts"`
+}
+
+func (c *Config) readConfig() {
+	f, err := os.Open("config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+//func SSHWorker(id int, conf SSHClient, sshhosts <-chan string, result chan<- []string) {
+//	for sshhost := range sshhosts {
+//
+//	}
+//}
+
+func ProcessSSHHost(conf SSHClient, sshhost string) ([]string, error) {
+	var client *sshclient.Client
+	var err error
+	if len(conf.KeyFile) > 0 {
+		client, err = sshclient.DialWithKey(sshhost+":22", conf.User, conf.KeyFile)
+	} else {
+		client, err = sshclient.DialWithPasswd(sshhost+":22", conf.User, conf.Pass)
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	out, err := client.Cmd("show configuration | display xml").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc, err := xmlquery.Parse(bytes.NewReader(out))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var ret []string
+	for _, res := range xmlquery.Find(doc, "//*[@inactive and not(ancestor::*[@inactive])]") {
+		ret = append(ret, sshhost+":"+GetPath(res))
+	}
+	return ret, nil
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
 	if len(args) < 1 {
 		log.Fatal("missing file as command argument")
 	}
+
+	var cfg Config
+	cfg.readConfig()
+
+	fmt.Printf("%v\n", cfg)
 
 	for _, filename := range args {
 		test, err := ioutil.ReadFile(filename)
